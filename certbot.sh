fa41d69af8d07f5a6deef5a960d974d3eaeaf8fa
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2023-01-18 16:43:07 -0300

    adicionando ssl ao chatbot

--- certbot.sh
+++ certbot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm --name certbot -v "/home/sistemas/proxy/certs:/etc/letsencrypt/live/marcoslauder.tech" certbot/certbot certonly --manual -d marcoslauder.tech
+docker run -it --rm --name certbot -v "/home/sistemas/proxy/certs:/etc/letsencrypt/archive" certbot/certbot certonly --manual -d marcoslauder.tech
