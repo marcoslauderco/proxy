7efe00dfa35ba2848f5b52b8d79c1858c39bbe54
marcoslauderco <30693799+marcoslauderco@users.noreply.github.com>
2024-09-24 15:11:49 -0300

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -it --rm --name certbot -v "/home/sistemas/proxy/certs:/etc/letsencrypt/archive" certbot/certbot certonly --manual -d marcoslauder.com.br,casamento.marcoslauder.com.br,chatbotcasamento.marcoslauder.com.br
+#docker run -it --rm --name certbot -v "/home/sistemas/proxy/certs:/etc/letsencrypt/archive" certbot/certbot certonly --manual -d marcoslauder.com.br,casamento.marcoslauder.com.br,chatbotcasamento.marcoslauder.com.br
+docker run -it --rm --name certbot -v "/home/sistemas/proxy/certs:/etc/letsencrypt/archive" certbot/certbot renew --quiet
